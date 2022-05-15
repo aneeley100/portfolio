f9fcbdc326e0e3045d8be3b06ea77c21a5d1f3dc
Andrew Neeley <aneeley05@gmail.com>
2022-05-14 21:12:16 -0700

    asdf

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { Stack, Typography, CssBaseline, Divider, Link } from '@mui/material';
+import { Stack, Typography, CssBaseline, Divider, GridClassKey, Grid, Box } from '@mui/material';
 import LinkIcon from '@mui/icons-material/Link';
 import { ThemeProvider } from '@emotion/react';
 import DarkTheme from './DarkTheme';
@@ -15,18 +15,22 @@ function App() {
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=DM+Serif+Display:ital@0;1&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=DM+Serif+Display:ital@0;1&family=Roboto+Mono&display=swap" rel="stylesheet" />
       </head>
-      <div className="App">
+      <Box
+        bgcolor="primary.main"
+        width="50rem"
+      >
         <div className="CenterContent">
           <div className='Hero'>
             <Stack spacing="2rem">
-              <Typography variant='h1'>Not Your Everyday<br />Web Developer</Typography>
-              <Typography variant='body1'>I'm a software engineer that specializes in building<br/>and designing fantastic digital experiences.</Typography>
+              <Typography variant="h6" color="primary">Hi, my name is</Typography>
+              <Typography variant='h1' color="secondary.light">Not Your Everyday<br />Web Developer</Typography>
+              <Typography variant='body1' color="secondary">I'm a software engineer that specializes in building<br />and designing fantastic digital experiences.</Typography>
             </Stack>
           </div>
         </div>
-      </div>
+      </Box>
     </ThemeProvider>
   );
 }
--- src/DarkTheme.js
+++ src/DarkTheme.js
@@ -1,5 +1,5 @@
-import { createTheme } from '@mui/material';
-
+import { createTheme, createMuiTheme } from '@mui/material';
+const { palette } = createMuiTheme();
 const DarkTheme = createTheme({
     dark: true,
     typography: {
@@ -10,6 +10,10 @@ const DarkTheme = createTheme({
             letterSpacing: '-0.0125em',
             lineHeight: '4rem',
         },
+        h6: {
+            fontFamily: 'Roboto Mono',
+            fontsize: '1rem',
+        },
         fontFamily: [
             "DM Sans",
             "sans-serif"
@@ -17,6 +21,19 @@ const DarkTheme = createTheme({
     },
     palette: {
         mode: 'dark',
+        primary: {
+            main: '#64ffda',
+        },
+        secondary: {
+            main: '#8892b0',
+            light: '#ccd6f6',
+        },
+        background: {
+                default: '#0a192f',
+                dark: '#020c1b',
+                light: '#112240',
+                ligtest: '#233554',
+        }
     }
 })
 
