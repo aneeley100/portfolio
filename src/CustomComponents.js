38254f6ecdd3f23fc602704e176066208bdebbfe
Andrew Neeley <aneeley05@gmail.com>
2022-05-16 23:25:08 -0700

    asdf

--- src/CustomComponents.js
+++ src/CustomComponents.js
@@ -38,7 +38,7 @@ function CustomFooter() {
         return (
             <footer>
                 <Stack>
-                    <Typography variant="caption" color={DarkTheme.palette.primary.dim}>Built by Andrew Neeley.</Typography>
+                    <Typography variant="caption" color={DarkTheme.palette.primary.dim + "80"}>Built by Andrew Neeley.</Typography>
                     <Typography variant="caption" color={DarkTheme.palette.primary.dim}>(Developer Mode)</Typography>
                 </Stack>
             </footer>
