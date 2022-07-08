f03643075130832fdd5f3988aeb1271a65953174
Andrew <41931259+aneeley05@users.noreply.github.com>
2022-07-08 17:46:44 +0000

    asdf

---
+++ pages/index.jsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material'
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <Typography>Hello World!</Typography>
+  )
+}
