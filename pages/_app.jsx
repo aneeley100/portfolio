bcc97d5ee9ed272725c2d8071742fda044fba1a8
Andrew <41931259+aneeley05@users.noreply.github.com>
2022-07-12 20:17:41 +0000

    asdf

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -59,7 +59,7 @@ function MyApp({ Component, pageProps }) {
       <CssBaseline />
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=DM+Serif+Display&family=Source+Code+Pro:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <Component {...pageProps} />
