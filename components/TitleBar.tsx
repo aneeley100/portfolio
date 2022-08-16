858b0a35f9dd757dca69866e236802b68fe8f903
Andrew Neeley <aneeley05@gmail.com>
2022-08-16 14:36:30 -0700

    sidebar changes

--- components/TitleBar.tsx
+++ components/TitleBar.tsx
@@ -2,17 +2,17 @@
 
 export default function TitleBar() {
 	return (
-		<div className='h-screen p-3 flex flex-col justify-between align-middle w-min text-right pl-32 py-32'>
+		<div className='h-[calc(100vh-12vh)] p-3 flex flex-col justify-between align-middle w-min text-center pl-6 pr-6 py-[3vh] my-[6vh] border-r-[1px] border-gray-700'>
 			<div
-				className='font-display font-bold text-6xl rotate-180 hover:text-navy-900 cursor-default transition-all duration-200'
+				className='font-display font-bold text-5xl rotate-180 hover:text-navy-900 cursor-default transition-all duration-200'
 				style={{
 					writingMode: "vertical-rl",
 					WebkitTextStrokeColor: "white",
 					WebkitTextStrokeWidth: "1px",
 				}}>
-				ANDREW NEELEY
+				aneeley<span className='opacity-20'>.dev</span>
 			</div>
-			<div>Links</div>
+			<div>L</div>
 		</div>
 	);
 }
