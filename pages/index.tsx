3b6fef073714d323f7c129202c4c3b4812c2a08b
Andrew Neeley <aneeley05@gmail.com>
2022-08-16 15:10:52 -0700

    comment out gsap for now

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,23 +7,23 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 export default function Home() {
 	const containerRef = useRef();
 
-	useEffect(() => {
-		gsap.to(containerRef.current, {
-			x: () =>
-				-(
-					containerRef.current.scrollWidth -
-					document.documentElement.clientWidth
-				) + "px",
-			ease: "none",
-			scrollTrigger: {
-				trigger: containerRef.current,
-				invalidateOnRefresh: true,
-				pin: true,
-				scrub: 1,
-				end: () => "+=" + containerRef.current.offsetWidth,
-			},
-		});
-	}, []);
+	// useEffect(() => {
+	// 	gsap.to(containerRef.current, {
+	// 		x: () =>
+	// 			-(
+	// 				containerRef.current.scrollWidth -
+	// 				document.documentElement.clientWidth
+	// 			) + "px",
+	// 		ease: "none",
+	// 		scrollTrigger: {
+	// 			trigger: containerRef.current,
+	// 			invalidateOnRefresh: true,
+	// 			pin: true,
+	// 			scrub: 1,
+	// 			end: () => "+=" + containerRef.current.offsetWidth,
+	// 		},
+	// 	});
+	// }, []);
 
 	return (
 		<div className='w-100 h-100 flex flex-row space-x-16' ref={containerRef}>
