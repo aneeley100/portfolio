98812510f4eca8fbe86013149c641f968e887ce4
Andrew Neeley <aneeley05@gmail.com>
2022-12-13 22:09:02 -0700

    going pack to more professional content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,20 +4,19 @@
     		<link rel="stylesheet" href="%sveltekit.assets%/global.css" />
 </svelte:head>
 <div id="homepage">
-    <h1>Turning ideas into experiences through software.</h1>
-    <h4>
-        > <span style="color: var(--code-1);">let</span> <span style="color: var(--code-2);">developer</span> = <span style="color: var(--code-1);">new</span> <span style="color: var(--code-2);">Developer</span>(<span style="color: var(--code-3);">"Andrew Neeley"</span>); <br>
-        > <br>
-        > <span style="color: var(--code-2);">developer</span>.<span style="color: var(--code-4);">set</span>(<span style="color: var(--code-3);">"experience"</span>, <span style="color: var(--code-3);">"7 years"</span>);<br>
-        > <span style="color: var(--code-2);">developer</span>.<span style="color: var(--code-4);">set</span>(<span style="color: var(--code-3);">"languages"</span>, [<span style="color: var(--code-3);">"Javascript"</span>, <span style="color: var(--code-3);">"Typescript"</span>, <span style="color: var(--code-3);">"Python"</span>, <span style="color: var(--code-3);">"Rust"</span>]); <br>
-        > <span style="color: var(--code-2);">developer</span>.<span style="color: var(--code-4);">set</span>(<span style="color: var(--code-3);">"description"</span>, <span style="color: var(--code-3);">"Full stack developer based in Phoenix, Arizona"</span>);<br>
-        > <br>
-        > <span style="color: var(--code-2);">console</span>.<span style="color: var(--code-4);">log</span>(<span style="color: var(--code-2);">developer</span>.<span style="color: var(--code-4);">getGoals</span>());<br>
-         <span style="color: #eaeaea">I'm currently looking to join a team of engineers passionate about building great products.</span><br>
-        <br>
-        > <span style="color: var(--code-2);">console</span>.<span style="color: var(--code-4);">log</span>(<span style="color: var(--code-2);">developer</span>.<span style="color: var(--code-4);">getSocials</span>());<br>
-        <b>If you're interested in working together, feel free to reach out! You can find me on <a href="https://github.com/aneeley05">Github</a>, <a href="https://www.linkedin.com/in/aneeley/">LinkedIn</a>, or <a href="mailto:aneeley05@gmail.com">Email</a> me directly.</b>
+    <h1>Making good ideas a reality through elegant software.</h1>
+<h4>
+        I'm <b style="font-weight: 800;">Andrew Neeley</b>, a full stack developer based in <b>Phoenix, Arizona</b> with over 7 years of experience.
+        I specialize in building <b>simple, intuitive, and beautiful</b> applications.
+        <br><br>
+        I'm currently looking to join a team of engineers passionate about building great products.
+        <br><br>
+        If you're interested in working together, feel free to reach out! You can find me on <a href="https://github.com/aneeley05">Github</a>, <a href="https://www.linkedin.com/in/aneeley/">LinkedIn</a>, or <a href="mailto:aneeley05@gmail.com">Email</a> me directly.
     </h4>
+    <h2>Featured Projects</h2>
+    <div class="featured-projects">
+
+    </div>
 </div>
 
 <style>
@@ -65,12 +64,11 @@
         font-size: min(1rem,3.5vw);
         font-family: "JetBrains Mono", monospace;
         font-weight: 400;
-        background-color: rgba(255, 255, 255, 0.05);
-        padding: 1rem;
-        border-radius: 8px;
         line-height: 1.8;
-        margin-bottom: 5rem;
+        margin-bottom: 4rem;
+        padding-bottom: 6rem;
         color: #eaeaeaaf;
+        border-bottom: #eaeaeaaf 1px solid;
     }
     :root {
         --code-1: salmon;
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,45 @@
+<svelte:head>
+    <title>Andrew Neeley | About</title>
+    <meta name="description" content="Andrew Neeley | Developer: About me">
+    		<link rel="stylesheet" href="%sveltekit.assets%/global.css" />
+</svelte:head>
+<div id="page">
+    <img src="/andrew.jpg" alt="Andrew Neeley" class="about-image">
+    <h4>Hi there! I'm Andrew and I am a full stack software developer. I grew up in Phoenix, Arizona and i've been located here ever since. I've been very passionate about various fields of technology from a very young age. 
+        <br><br>
+        From software development to electrical engineering to mechanical engineering, my experience is diverse.
+        <br><br>
+        Outside of technology I enjoy athletics and music. I'm always looking for challenges and puzzles to push myself and reach new boundaries. I hope to continue to use my skills to create products i'm passionate about and find new ways to solve unique problems.
+    </h4>
+</div>
+
+<style>
+    #page {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: flex-start;
+        
+        width: min(80vw, 900px);
+        margin: 0 auto;
+        padding-top: 5rem;
+    }
+    .about-image {
+        width: min(100%, 600px);
+        height: 100%;
+        align-self: center;
+        object-fit: cover;
+        border-radius: 8px;
+        border: 1px solid rgba(255, 255, 255, 0.303);
+    }
+    h4 {
+        font-size: min(1rem,3.5vw);
+        font-family: "JetBrains Mono", monospace;
+        font-weight: 400;
+        padding: 1rem;
+        border-radius: 8px;
+        line-height: 1.8;
+        margin-bottom: 2rem;
+        color: #eaeaeab9;
+    }
+</style>
